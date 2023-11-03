cd959d4bec1d77126f18553c10093bef201d78ac
Y Trinh <trinhnhay@gmail.com>
2023-11-03 12:41:36 -0500

    add picture button

--- src/DashBoard/DashBoardPanel.java
+++ src/DashBoard/DashBoardPanel.java
@@ -5,15 +5,21 @@ import java.awt.*;
 
 public class DashBoardPanel extends JPanel {
     JPanel sidePanel;
+    PhotoArea photoPanel = new PhotoArea();
+
     public DashBoardPanel(){
-        setBackground(Color.ORANGE);
+        setBackground(new Color(241,148,184));
         setSidePanel();
     }
     private void setSidePanel(){
+
         sidePanel = new JPanel();
-        sidePanel.setBackground(Color.MAGENTA);
+        sidePanel.setPreferredSize(new Dimension(50, 300));
+        sidePanel.add(photoPanel.getPhotoPanel());
+        sidePanel.setBackground(new Color(248,184,208));
     }
     public JPanel getSidePanel(){
         return sidePanel;
     }
+
 }
