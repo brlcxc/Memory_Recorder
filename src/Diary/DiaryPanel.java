d6fbc7686a6313ac54d5b51e5260ce0a54d7d2b8
Bishop Lohman <brlcxc@umsystem.edu>
2023-11-29 14:10:51 -0600

    working on diary and list

--- src/Diary/DiaryPanel.java
+++ src/Diary/DiaryPanel.java
@@ -1,23 +1,32 @@
 package Diary;
 import Defaults.*;
-import Main.*;
+
 import javax.swing.*;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
+import javax.swing.plaf.metal.MetalBorders;
 import java.awt.*;
 import java.awt.event.*;
 import java.util.Hashtable;
 import java.util.Map;
+import java.util.Objects;
 
 public class DiaryPanel extends JPanel {
     private JPanel sidePanel;
     private final JTextArea textArea;
+    private final JTextArea dateTextArea;
+
     private final JTextField titleField;
     private JTextField searchField;
     private final Map<String, String> diaryEntryMap;
     private DefaultListModel<String> entryListModel;
     private JList<String> entryList;
     private String currentEntry;
+    private JPanel titlePanel;
+    private JButton saveIconButton;
+    private JButton editIconButton;
+    private JButton cancelIconButton;
+    private int currentIndex;
     public DiaryPanel(){
         GridBagConstraints gbc = new GridBagConstraints();
         diaryEntryMap = new Hashtable<>();
@@ -26,11 +35,32 @@ public class DiaryPanel extends JPanel {
         setBackground(Colors.cream);
         setSidePanel();
 
+        //title panel
+        titlePanel = new JPanel();
+        titlePanel.setBackground(Colors.cream);
+
         //title field
         titleField = new DiaryTextField("Press \"Create New Entry\" to begin writing");
         titleField.setBorder(null);
         titleField.setBackground(Colors.cream);
         titleField.setHorizontalAlignment(JTextField.CENTER);
+        titleField.setPreferredSize(new Dimension(358, 24));
+
+        saveIconButton = iconButton("save.png",13,13);
+        saveIconButton.addActionListener(new SaveTitleButtonListener());
+
+        editIconButton = iconButton("editing.png",15,15);
+        editIconButton.addActionListener(new EditTitleButtonListener());
+
+        cancelIconButton = iconButton("cancel.png",15,15);
+        cancelIconButton.addActionListener(new CancelTitleButtonListener());
+
+        titlePanel.add(titleField);
+        titlePanel.add(saveIconButton);
+        titlePanel.add(editIconButton);
+        titlePanel.add(cancelIconButton);
+        HideButtons();
+
 
         //diary text area
         textArea = new JTextArea(16, 40);
@@ -39,12 +69,20 @@ public class DiaryPanel extends JPanel {
         textArea.setEditable(false);
         JScrollPane textAreaScroll = new JScrollPane(textArea);
 
+        //date text area
+        dateTextArea = new JTextArea(1, 40);
+        dateTextArea.setFont(new Font("SansSerif", Font.PLAIN, 16));
+        dateTextArea.setEditable(false);
+        dateTextArea.setVisible(false);
+        dateTextArea.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
+        dateTextArea.setBorder(MetalBorders.getTextFieldBorder());
+
         //save button
-        JButton saveButton = new DiaryButton("Save", Colors.pastelGreen, Colors.mintGreen);
+        JButton saveButton = new DiaryButton("Save Entry", Colors.pastelGreen, Colors.mintGreen);
         saveButton.addActionListener(new SaveButtonListener());
 
         //delete button
-        JButton deleteButton = new DiaryButton("Delete", Colors.barbiePink, Colors.lessBarbiePink);
+        JButton deleteButton = new DiaryButton("Delete Entry", Colors.barbiePink, Colors.lessBarbiePink);
         deleteButton.addActionListener(new DeleteButtonListener());
 
         //button panel
@@ -59,7 +97,7 @@ public class DiaryPanel extends JPanel {
         gbc.gridy = 0;
         gbc.weightx = 1;
         gbc.fill = GridBagConstraints.HORIZONTAL;
-        add(titleField, gbc);
+        add(titlePanel, gbc);
 
         gbc.insets = new Insets(8,8,8,8);
         gbc.gridy = -1;
@@ -68,10 +106,15 @@ public class DiaryPanel extends JPanel {
         gbc.fill = GridBagConstraints.BOTH;
         add(textAreaScroll, gbc);
 
+        gbc.insets = new Insets(0,8,8,8);
+        gbc.weighty = 0;
+        gbc.gridy = -2;
+        add(dateTextArea, gbc);
+
         gbc.insets = new Insets(0,8,2,8);
         gbc.weightx = 0;
         gbc.weighty = 0;
-        gbc.gridy = -1;
+        gbc.gridy = -3;
         gbc.fill = GridBagConstraints.NONE;
         add(buttonPanel, gbc);
     }
@@ -99,7 +142,7 @@ public class DiaryPanel extends JPanel {
         entryList = new JList<>(entryListModel);
         entryList.setFixedCellHeight(30);
         entryList.setFont(new Font("SansSerif", Font.PLAIN, 18));
-        entryList.addMouseListener(new mouseListener());
+        entryList.addMouseListener(new mouseListener());;
         JScrollPane listScroller = new JScrollPane(entryList);
 
         //create new entry button
@@ -136,6 +179,8 @@ public class DiaryPanel extends JPanel {
     }
     private class CreateNewButtonListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
+            System.out.println(titleField.getWidth());
+            System.out.println(titleField.getHeight());
             String newEntry;
             if(!entryListModel.contains("<<New Entry>>")){
                 newEntry = "<<New Entry>>";
@@ -151,19 +196,33 @@ public class DiaryPanel extends JPanel {
                 }
             }
             //element added to list
+//            entryListModel.addElement(newEntry + " (2/24/23)");
             entryListModel.addElement(newEntry);
             //element added to map
             diaryEntryMap.put(newEntry, "");
-
             //global current entry changed
             currentEntry = newEntry;
+            currentIndex = entryListModel.size() - 1;
 
             //text field and text area properly updated
             titleField.setText(currentEntry);
             textArea.setText(diaryEntryMap.get(currentEntry));
+            titleField.setPreferredSize(new Dimension(340, 24));
+
+
+            //modification buttons visible
+            ShowButtons();
+
+            dateTextArea.setVisible(true);
+            dateTextArea.setText("Last Edit: 2/24/23  11:32");
+
+            int[] g = {currentIndex};
+            entryList.setSelectedIndices(g);
 
             //text area active
             textArea.setEditable(true);
+            titleField.setEditable(false);
+            titleField.setBorder(null);
         }
     }
     private class DeleteButtonListener implements ActionListener {
@@ -177,7 +236,8 @@ public class DiaryPanel extends JPanel {
             //statement called if selected index is not the first
             if(selectedIndex > 0) {
                 //the list item one index prior is selected
-                int[] g = {selectedIndex - 1};
+                currentIndex = selectedIndex - 1;
+                int[] g = {currentIndex};
                 entryList.setSelectedIndices(g);
                 //global entry updated
                 currentEntry = entryList.getModel().getElementAt(selectedIndex - 1);
@@ -188,7 +248,8 @@ public class DiaryPanel extends JPanel {
             //statement called if the index is 0 and there is at least one element left
             else if (entryListModel.size() > 0) {
                 //first index of list is selected
-                int[] g = {0};
+                currentIndex = 0;
+                int[] g = {currentIndex};
                 entryList.setSelectedIndices(g);
                 //global entry updated
                 currentEntry = entryList.getModel().getElementAt(0);
@@ -199,10 +260,16 @@ public class DiaryPanel extends JPanel {
             //statement called if there are no elements remaining after deletion
             else{
                 //text set to default state
+                currentIndex = -1;
                 titleField.setText("Press \"Create New Entry\" to begin writing");
+                titleField.setPreferredSize(new Dimension(358, 24));
                 textArea.setText("");
                 textArea.setEditable(false);
+                dateTextArea.setVisible(false);
+                HideButtons();
             }
+            titleField.setEditable(false);
+            titleField.setBorder(null);
         }
     }
     private class SaveButtonListener implements ActionListener {
@@ -210,18 +277,50 @@ public class DiaryPanel extends JPanel {
             // size > 0 accounts for clicking on empty list
             if(entryListModel.size() > 0) {
                 if (!currentEntry.equals(titleField.getText().trim())) {
-                    //new entry name added to list
-                    entryListModel.addElement(titleField.getText());
+                    //modified entry name changed at index
+//                    entryListModel.setElementAt(titleField.getText() + " (2/24/23)", currentIndex);
+                    entryListModel.setElementAt(titleField.getText(), currentIndex);
                     //old entry removed from map
                     diaryEntryMap.remove(currentEntry);
-                    //old entry name removed from list by index
-                    entryListModel.remove(entryListModel.indexOf(currentEntry));
                     //global current entry changed
                     currentEntry = titleField.getText();
                 }
                 //The entry is updated in the map if it has the same name
                 //The entry is added to the map if it has a new name
                 diaryEntryMap.put(currentEntry, textArea.getText());
+            }
+        }
+    }
+    private class EditTitleButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            titleField.setEditable(true);
+            titleField.setBorder(BorderFactory.createLineBorder(Colors.pastelPurple, 1));
+        }
+    }
+    private class CancelTitleButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            titleField.setEditable(false);
+            titleField.setBorder(null);
+            titleField.setText(currentEntry);
+        }
+    }
+    private class SaveTitleButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            titleField.setEditable(false);
+            titleField.setBorder(null);
+
+            if (!currentEntry.equals(titleField.getText().trim())) {
+                //modified entry name changed at index
+//                entryListModel.setElementAt(titleField.getText() + " (2/24/23)", currentIndex);
+                entryListModel.setElementAt(titleField.getText(), currentIndex);
+                //text extracted from old entry
+                String entryText = diaryEntryMap.get(currentEntry);
+                //old entry removed from map
+                diaryEntryMap.remove(currentEntry);
+                //global current entry changed
+                currentEntry = titleField.getText();
+                //New entry name added to map
+                diaryEntryMap.put(currentEntry, entryText);
             }
         }
     }
@@ -232,9 +331,16 @@ public class DiaryPanel extends JPanel {
             if (index >= 0) {
                 //global entry is changed to the selected entry
                 currentEntry = theList.getModel().getElementAt(index).toString();
+                currentIndex = index;
                 //text updated to represent selected entry
                 titleField.setText(currentEntry);
                 textArea.setText(diaryEntryMap.get(currentEntry));
+
+                //title editing disabled
+                titleField.setEditable(false);
+                titleField.setBorder(null);
+                titleField.setPreferredSize(new Dimension(340, 24));
+
                 //The text area is now active
                 textArea.setEditable(true);
             }
@@ -273,10 +379,21 @@ public class DiaryPanel extends JPanel {
             //elements containing the filter text are added to the list
             else {
                 if (!model.contains(dictionaryKey)) {
+//                    model.addElement(dictionaryKey + " (2/24/23)");
                     model.addElement(dictionaryKey);
                 }
             }
         }
+    }
+    public void HideButtons(){
+        saveIconButton.setVisible(false);
+        editIconButton.setVisible(false);
+        cancelIconButton.setVisible(false);
+    }
+    public void ShowButtons(){
+        saveIconButton.setVisible(true);
+        editIconButton.setVisible(true);
+        cancelIconButton.setVisible(true);
     }
     private static class SearchFocusListener implements FocusListener{
         //focus gained refers to when the text area either has text or is selected
@@ -295,4 +412,20 @@ public class DiaryPanel extends JPanel {
             }
         }
     }
+    private JButton iconButton(String icon, int width, int height){
+        ImageIcon image = new ImageIcon(Objects.requireNonNull(getClass().getResource(icon)));
+        Image img = image.getImage();
+        Image newImg = img.getScaledInstance(15, 15, Image.SCALE_SMOOTH);
+        JButton iconButton = new JButton(new ImageIcon(newImg));
+        iconButton.setPreferredSize(new Dimension(30, 30));
+        iconButton.setBorder(BorderFactory.createEmptyBorder());
+        iconButton.setBackground(Colors.cream);
+        iconButton.setFocusable(false);
+        return iconButton;
+    }
 }
+//maybe split up the panel further
+//have a check box showing completing?
+//maybe have a last edit in bottom right corner that appears as part of box but isn't
+
+//I wonder if there is a way too cut off text at a certain point but still have the end part
--- src/ListToDo/ListToDoPanel.java
+++ src/ListToDo/ListToDoPanel.java
@@ -1,13 +1,13 @@
 package ListToDo;
 
-import Defaults.Colors;
-import Defaults.ToDoButton;
-import Defaults.ToDoLabel;
+import Defaults.*;
+import Diary.DiaryPanel;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Hashtable;
 
 public class ListToDoPanel extends JPanel {
     JPanel sidePanel;
@@ -16,9 +16,13 @@ public class ListToDoPanel extends JPanel {
     private JList<String> list1;
     private JList<String> list2;
     private JTextField inputField;
+    private JTextField searchField;
+    private DefaultListModel<String> entryListModel;
+    private JList<String> entryList;
     public ListToDoPanel(){
-        setBackground(Color.YELLOW);
+/*
         setLayout(new BorderLayout());
+        setBackground(Colors.cream);*/
 
         listModel = new DefaultListModel<>();
         list1 = new JList<>(listModel);
@@ -28,7 +32,7 @@ public class ListToDoPanel extends JPanel {
 
         inputField = new JTextField();
         ToDoButton addButton = new ToDoButton("Add", Colors.pastelGreen, Colors.mintGreen);
-        ToDoButton deleteButton = new ToDoButton("Delete", Colors.barbiePink, Colors.lessBarbiePink);
+        ToDoButton deleteButton = new ToDoButton("Remove Item", Colors.barbiePink, Colors.lessBarbiePink);
 
         addButton.addActionListener(new AddButtonListener());
         deleteButton.addActionListener(new DeleteButtonListener());
@@ -37,9 +41,11 @@ public class ListToDoPanel extends JPanel {
                 BorderFactory.createLineBorder(Color.GRAY, 2),
                 BorderFactory.createEmptyBorder(5, 5, 5, 5)));
         inputField.setFont(new Font("SansSerif", Font.PLAIN, 18));
+        inputField.setText("Add new list item");
 
         // Top panel with input field and add button
         JPanel inputPanel = new JPanel();
+        inputPanel.setBackground(Colors.cream);
         inputPanel.setLayout(new BorderLayout());
         inputPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 0, 10));
         inputPanel.add(inputField, BorderLayout.CENTER);
@@ -47,23 +53,81 @@ public class ListToDoPanel extends JPanel {
 
         // Bottom panel with delete button
         JPanel bottomPanel = new JPanel();
+        bottomPanel.setBackground(Colors.cream);
         bottomPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
         bottomPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
         bottomPanel.add(deleteButton);
 
-        add(inputPanel, BorderLayout.NORTH);
+/*        add(inputPanel, BorderLayout.NORTH);
         add(listScroller, BorderLayout.CENTER);
         add(bottomPanel, BorderLayout.SOUTH);
 
 
+        setSidePanel();*/
+        GridBagConstraints gbc = new GridBagConstraints();
+//        diaryEntryMap = new Hashtable<>();
+
+        setLayout(new GridBagLayout());
+        setBackground(Colors.cream);
         setSidePanel();
+
+/*
+        //title field
+        titleField = new DiaryTextField("Press \"Create New Entry\" to begin writing");
+        titleField.setBorder(null);
+        titleField.setBackground(Colors.cream);
+        titleField.setHorizontalAlignment(JTextField.CENTER);
+
+        //diary text area
+        textArea = new JTextArea(16, 40);
+        textArea.setFont(new Font("SansSerif", Font.PLAIN, 16));
+        textArea.setLineWrap(true);
+        textArea.setEditable(false);
+        JScrollPane textAreaScroll = new JScrollPane(textArea);
+
+        //save button
+        JButton saveButton = new DiaryButton("Save", Colors.pastelGreen, Colors.mintGreen);
+        saveButton.addActionListener(new DiaryPanel.SaveButtonListener());
+
+        //delete button
+        JButton deleteButton = new DiaryButton("Delete", Colors.barbiePink, Colors.lessBarbiePink);
+        deleteButton.addActionListener(new DiaryPanel.DeleteButtonListener());
+
+        //button panel
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setBackground(Colors.cream);
+        buttonPanel.add(saveButton);
+        buttonPanel.add(Box.createHorizontalStrut(20));
+        buttonPanel.add(deleteButton);
+*/
+
+        gbc.insets = new Insets(8,8,0,8);
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.weightx = 1;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        add(inputPanel, gbc);
+
+        gbc.insets = new Insets(8,8,8,8);
+        gbc.gridy = -1;
+        gbc.weightx = 1;
+        gbc.weighty = 1;
+        gbc.fill = GridBagConstraints.BOTH;
+        add(listScroller, gbc);
+
+        gbc.insets = new Insets(0,8,2,8);
+        gbc.weightx = 0;
+        gbc.weighty = 0;
+        gbc.gridy = -1;
+        gbc.fill = GridBagConstraints.NONE;
+        add(bottomPanel, gbc);
     }
     private void setSidePanel(){
-        sidePanel = new JPanel();
+/*        sidePanel = new JPanel();
         sidePanel.setLayout(new BorderLayout());
         ToDoButton addButton = new ToDoButton("Create New", Colors.pastelGreen, Colors.mintGreen);
         ToDoLabel test = new ToDoLabel("Date");
-//        sidePanel.setBackground(Color.GREEN);
+        sidePanel.setBackground(Colors.cream);
         sidePanel.add(new JLabel("help"));
         listModel2 = new DefaultListModel<>();
         list2 = new JList<>(listModel);
@@ -72,9 +136,62 @@ public class ListToDoPanel extends JPanel {
         JScrollPane listScroller = new JScrollPane(list2);
         sidePanel.add(test, BorderLayout.NORTH);
         sidePanel.add(listScroller, BorderLayout.CENTER);
-        sidePanel.add(addButton, BorderLayout.SOUTH);
+        sidePanel.add(addButton, BorderLayout.SOUTH);*/
 
+        GridBagConstraints gbc = new GridBagConstraints();
+        sidePanel = new JPanel();
+        sidePanel.setLayout(new GridBagLayout());
+        sidePanel.setBackground(Colors.cream);
 
+        //entries label
+        JLabel entriesLabel = new DiaryLabel("Available Lists", 18);
+
+        //search field
+        searchField = new DiarySearchField();
+/*        searchField.getDocument().addDocumentListener(new DiaryPanel.SearchDocumentListener());
+        searchField.addFocusListener(new DiaryPanel.SearchFocusListener());*/
+
+        //search field label
+        JLabel searchLabel = new JLabel("");
+        searchLabel.setForeground(Colors.textColor);
+        searchLabel.setFont(new Font("SansSerif", Font.PLAIN, 11));
+
+        //list and list scroller
+        entryListModel = new DefaultListModel<>();
+        entryList = new JList<>(entryListModel);
+        entryList.setFixedCellHeight(30);
+        entryList.setFont(new Font("SansSerif", Font.PLAIN, 18));
+//        entryList.addMouseListener(new DiaryPanel.mouseListener());
+        JScrollPane listScroller = new JScrollPane(entryList);
+
+        //create new entry button
+        JButton createNewButton = new DiaryButton("Create New List", Colors.pastelGreen, Colors.mintGreen);
+//        createNewButton.addActionListener(new DiaryPanel.CreateNewButtonListener());
+
+        gbc.insets = new Insets(8,8,0,8);
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        sidePanel.add(entriesLabel, gbc);
+
+        gbc.insets = new Insets(8,8,0,0);
+        gbc.weightx = 1;
+        gbc.gridy = -1;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        sidePanel.add(searchField, gbc);
+
+        gbc.insets = new Insets(0,8,8,0);
+        gbc.gridy = -2;
+        gbc.weightx = 1;
+        gbc.weighty = 1;
+        gbc.fill = GridBagConstraints.BOTH;
+        sidePanel.add(listScroller, gbc);
+
+        gbc.insets = new Insets(4,8,8,8);
+        gbc.gridy = -3;
+        gbc.weightx = 0;
+        gbc.weighty = 0;
+        gbc.fill = GridBagConstraints.NONE;
+        sidePanel.add(createNewButton, gbc);
     }
     public JPanel getSidePanel(){
         return sidePanel;
